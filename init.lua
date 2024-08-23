3a756aa03b2439fa9842a6bc6ad22a20d6fdf21e
sidvyass <sidd.vyass@gmail.com>
2024-08-22 20:59:11 -0400

    giga chad theme installed, comments enabled, disabled annoying comment shortcut

--- init.lua
+++ init.lua
@@ -92,6 +92,8 @@ end)
 vim.keymap.set('n', '<C-s>', function()
   ui.nav_file(1)
 end)
+
+vim.keymap.set('v', '<C-_>', '<Plug>(comment_toggle_linewise_visual)', { desc = 'Comment out code' })
 -- my configs ----------------------------
 
 -- [[ Basic Keymaps ]]
@@ -163,6 +165,17 @@ require('lazy').setup({
   'MunifTanjim/nui.nvim',
   'nvim-lua/plenary.nvim',
   {
+    'numToStr/Comment.nvim',
+    opts = {
+      opleader = {
+        ---Line-comment keymap
+        -- line = 'gc',
+        ---Block-comment keymap
+        block = 'gb',
+      },
+    },
+  },
+  {
     'nvim-neo-tree/neo-tree.nvim',
     branch = 'v2.x',
     requires = {
@@ -178,11 +191,23 @@ require('lazy').setup({
             hide_gitignored = false,
           },
         },
+        window = {
+          mappings = {
+            ['s'] = 'open_split',
+            ['v'] = 'open_vsplit',
+          },
+        },
+        event_handlers = {
+          {
+            event = 'file_opened',
+            handler = function()
+              require('neo-tree.command').execute { action = 'close' }
+            end,
+          },
+        },
       }
     end,
   },
-  --  'nvim-neo-tree/neo-tree.nvim',
-  --  'MunifTanjim/nui.nvim', -- this is a dependency for above
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -717,15 +742,71 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'catppuccin/nvim',
+    -- 'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
+    config = function()
+      require('catppuccin').setup {
+
+        flavour = 'auto', -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = 'latte',
+          dark = 'mocha',
+        },
+        transparent_background = true, -- disables setting the background color.
+        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+        dim_inactive = {
+          enabled = true, -- dims the background color of inactive window
+          shade = 'dark',
+          percentage = 0.15, -- percentage of the shade to apply to the inactive window
+        },
+        no_italic = false, -- Force no italic
+        no_bold = false, -- Force no bold
+        no_underline = false, -- Force no underline
+        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { 'italic' }, -- Change the style of comments
+          conditionals = { 'italic' },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+          -- miscs = {}, -- Uncomment to turn off hard-coded styles
+        },
+        color_overrides = {
+          mocha = {
+            base = '#000000',
+            mantle = '#000000',
+            crust = '#000000',
+          },
+        },
+        custom_highlights = {},
+        default_integrations = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = '',
+          },
+        },
+      }
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'catppuccin-mocha'
 
       -- You can configure highlights by doing something like:
+      -- vim.cmd.hi('Comment', 'guifg=#FF8800', 'guibg=NONE', 'gui=italic')
       vim.cmd.hi 'Comment gui=none'
     end,
   },
